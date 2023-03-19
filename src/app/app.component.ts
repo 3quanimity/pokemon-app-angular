aaaf1b56716af09fc114db69fe30053cd3171001
Haythem <sinnner6@gmail.com>
2023-03-19 12:41:16 +0100

    playing with events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,26 @@ import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Liste de Pokemons!</h1>`,
+  templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[5]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`you have clicked on ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon = this.pokemonList.find(
+      (pokemon) => pokemon.id === Number(pokemonId)
+    );
+    if (pokemon) {
+      console.log(`The selected pokemon is ${pokemon?.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log("No pokemon was found");
+      this.pokemonSelected = pokemon;
+    }
   }
 }
