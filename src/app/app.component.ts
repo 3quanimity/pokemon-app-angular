23cf2d466b3ac98ea7ef04018751a9db2acbce5f
Haythem <sinnner6@gmail.com>
2023-03-19 19:10:22 +0100

    added dynamic card coloring depending on pokemon first type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,4 +37,41 @@ export class AppComponent implements OnInit {
       this.filteredPokemons = this.pokemonList;
     }
   }
+
+  getTypeColor(type: string): string {
+    switch (type) {
+      case "Water":
+        return "#79bef2";
+      case "Fire":
+        return "#ff6961";
+      case "Electric":
+        return "#ffe493";
+      case "Poison":
+        return "#ba55d3";
+      case "Grass":
+        return "#8bcd7d";
+      case "Ice":
+        return "#add8e6";
+      case "Flying":
+        return "#b0c4de";
+      case "Bug":
+        return "#b8d0a5";
+      case "Ground":
+        return "#d3b357";
+      case "Rock":
+        return "#a6937c";
+      case "Ghost":
+        return "#c5aecf";
+      case "Dragon":
+        return "#f9a1c2";
+      case "Psychic":
+        return "#ffb6c1";
+      case "Fighting":
+        return "#cd5c5c";
+      case "Normal":
+        return "#f5deb3";
+      default:
+        return "#fff";
+    }
+  }
 }
