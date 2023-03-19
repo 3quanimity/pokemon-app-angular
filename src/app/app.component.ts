839f93dfb82e6a068d5a549ec9f2584019bf43a6
Haythem <sinnner6@gmail.com>
2023-03-19 10:41:10 +0100

    playing with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemons";
+import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: `<h1>Welcome to {{ title }}!</h1>`,
+  template: `<h1>Liste de Pokemons!</h1>`,
 })
-export class AppComponent {
-  title = "ng-pokemon-app";
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[5]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`you have clicked on ${pokemon.name}`);
+  }
 }
