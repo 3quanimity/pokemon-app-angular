407864f89e564055444902fbc431d9bcddaa226c
Haythem <sinnner6@gmail.com>
2023-03-21 19:34:13 +0100

    removed unnecessary comments

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -8,7 +8,6 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemonList(): Observable<Pokemon[] | []> {
-    // return POKEMONS;
     return this.http.get<Pokemon[]>("api/pokemons").pipe(
       tap((pokemonList) => this.log(pokemonList)),
       catchError((error) => this.handleError(error, []))
@@ -16,7 +15,6 @@ export class PokemonService {
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
-    // return POKEMONS.find((pokemon) => pokemon.id === pokemonId);
     return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
       tap((pokemon) => this.log(pokemon)),
       catchError((error) => this.handleError(error, undefined))
